2e741c301681345121fd3f84b478f306650f9284
David Rowswell <davi144@mail.ubc.ca>
2018-10-25 13:46:20 -0700

    Adding a test folder for testing with the collections server

---
+++ testThings/testThings.php
@@ -0,0 +1,20 @@
+<?php
+require_once ('FileMaker.php');
+
+$fm = new FileMaker();
+$fm->setProperty('database', 'Avian Research Collection.fmp12');
+$fm->setProperty('hostspec', 'https://collections.zoology.ubc.ca');
+$fm->setProperty('username', 'admin');
+$fm->setProperty('password', 'admin');
+
+$connected = $fm->listLayouts();
+If(FileMaker::isError($connected)){
+        echo "out of luck ".$connected;
+} else {
+        echo "connected <br>";
+        foreach($connected as $i){
+                echo $i . "<br>";
+        }
+}
+
+?>

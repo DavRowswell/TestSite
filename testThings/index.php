f128aa064b8e4825588510661681d73520a2cb35
h8p1b <stevensqiu@gmail.com>
2018-11-21 15:05:43 -0800

    testing queries on MIW database

--- testThings/index.php
+++ testThings/index.php
@@ -1,18 +1,42 @@
 <?php
 require_once ('FileMaker.php');
+require_once ('db.php');
+// $fm = new FileMaker();
+// require_once 'db.php';
 
-$fm = new FileMaker();
-include('db.php');
+$fm = new FileMaker(FM_FILE, FM_HOST, FM_USER, FM_PASS);
 
 $connected = $fm->listLayouts();
+$findAllObject = $fm->newFindAllCommand($connected[0]);
+echo $connected[0] . "<br>";
+$findAllRes = $findAllObject->execute();
+$findAllRec = $findAllRes->getRecords();
 If(FileMaker::isError($connected)){
         echo "out of luck ".$connected;
-} else {
-        echo "connected <br>";
-        foreach($connected as $i){
-                echo $i . "<br>";
-        }
-        echo "crystal is here <br>";
+// } else {
+//         $recFields = $findAllRes->getFields();
+//         echo "connected <br>";
+//         foreach($findAllRec as $i){
+//                 foreach($recFields as $j){
+//                         echo $i->getField($j) . " ";
+//                 }
+//                 echo "<br>";
+//         }
+        // foreach($fm->listScripts() as $db){
+        //         echo $db . "<br>";
+        // }
 }
 
 ?>
+
+<html>
+<body>
+
+<form action="search.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>

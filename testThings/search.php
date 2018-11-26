ac4d5f3221514639ed54159eec13e5e22db45d36
crystal-ck7 <crystal.ck7@gmail.com>
2018-11-26 15:42:55 -0800

    included navbar

--- testThings/search.php
+++ testThings/search.php
@@ -1,5 +1,12 @@
 <!-- Bootstrap CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
 <div class="container">
 <?php
 
@@ -34,9 +41,25 @@ $layouts = $fm->listLayouts();
 ?>
 
 <html>
-<body>
 
-identificationID: <?php echo $_GET["name"]; ?><br>
+
+<body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">HerbariumDB</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
+
+
+<!-- identificationID: <?php echo $_GET["name"]; ?><br> -->
 <!-- Your email address is: <?php // echo $_GET["email"]; ?> -->
 
 <?php 

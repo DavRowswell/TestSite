2837fc097b90a585a2a77d83a9c579c3d5fd916e
crystal-ck7 <crystal.ck7@gmail.com>
2018-11-26 15:28:04 -0800

    changed table headings

--- testThings/search.php
+++ testThings/search.php
@@ -1,3 +1,6 @@
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<div class="container">
 <?php
 
 require_once ('FileMaker.php');
@@ -35,33 +38,60 @@ $layouts = $fm->listLayouts();
 
 identificationID: <?php echo $_GET["name"]; ?><br>
 <!-- Your email address is: <?php // echo $_GET["email"]; ?> -->
+
 <?php 
-
-
 $findCommand = $fm->newFindCommand($layouts[2]);
 $identificationID = $_GET["name"];
-$findCommand->addFindCriterion('identificationID', $identificationID);
+$findCommand->addFindCriterion('Accession No.', $identificationID);
 $result = $findCommand->execute(); 
 $findAllRec = $result->getRecords();
 If(FileMaker::isError($layouts)){
     echo "out of luck ".$layouts;
 } else {
     $recFields = $result->getFields();
+    ?>
 
-    foreach($recFields as $l){
-        echo $l . " ";
-    }
-    echo "<br>";
+    <table class="table">
+  <thead>
+    <tr>
+    <?php foreach($recFields as $i){?>
+      <th scope="col"><?php echo $i ?></th>
+    <?php }?>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach($findAllRec as $i){
+        if (strlen($_GET['name']) == strlen($i->getField('Accession No.'))) {
+    ?>
+    <tr>
+      <?php foreach($recFields as $j){?>
+      <td><?php echo $i->getField($j) ?></td>
+      <?php }?>
+    </tr>
+      <?php }}?>
+  </tbody>
+</table>
+    
+    <?php
+
+
+
+    // $recFields = $result->getFields();
+
+    // foreach($recFields as $l){
+    //     echo $l . " ";
+    // }
+    // echo "<br>";
 
     // echo "connected <br>";
-    foreach($findAllRec as $i){
-        if (strlen($_GET['name']) == strlen($i->getField('identificationID'))) {
-            foreach($recFields as $j){
-                echo $i->getField($j) . " ";
-            }
-                echo "<br>";
-        }
-    }
+    // foreach($findAllRec as $i){
+    //     if (strlen($_GET['name']) == strlen($i->getField('identificationID'))) {
+    //         foreach($recFields as $j){
+    //             echo $i->getField($j) . " ";
+    //         }
+    //             echo "<br>";
+    //     }
+    // }
     // foreach($fm->listScripts() as $db){
     //         echo $db . "<br>";
     // }
@@ -69,3 +99,6 @@ If(FileMaker::isError($layouts)){
 ?>
 </body>
 </html>
+
+  <!-- Content here -->
+  </div>

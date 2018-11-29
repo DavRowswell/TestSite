768e6d0968e0442dfc507b5a858bad241784002e
h8p1b <stevensqiu@gmail.com>
2018-11-28 16:48:50 -0800

    adding compoundFind functionality

--- testThings/functions.php
+++ testThings/functions.php
@@ -2,12 +2,38 @@
 require_once ('FileMaker.php');
 require_once ('db.php');
 
-$fm = new FileMaker($FM_FILE, $FM_HOST, $FM_USER, $FM_PASS);
+$fm = new FileMaker(FM_FILE, FM_HOST, FM_USER, FM_PASS);
 
 $layouts = $fm->listLayouts();
+
+// CompoundFind on all inputs with values
+// $compoundFind = $fm->newCompoundFindCommand($layouts[2]);
+
+// $accessionFindRequest = $fm->newFindRequest($layouts[2]);
+// $accessionFindRequest->addFindCriterion('Accession No.', '=='.$_GET['Accession No.']);
+
+// $genusFindRequest = $fm->newFindRequest($layouts[2]);
+// $genusFindRequest->addFindCriterion('Genus', '=='.$_GET['Genus']);
+
+// $speciesFindRequest = $fm->newFindRequest($layouts[2]);
+// $speciesFindRequest->addFindCriterion('Species', '=='.$_GET['Species']);
+
+// $locationFindRequest = $fm->newFindRequest($layouts[2]);
+// $locationFindRequest->addFindCriterion('Location', '=='.$_GET['Location']);
+
+// $compoundFind->add($accessionFindRequest);
+// $compoundFind->add($genusFindRequest);
+// $compoundFind->add($speciesFindRequest);
+// $compoundFind->add($locationFindRequest);
+
+// $compoundFind->addSortRule('Accession No.', 1, FILEMAKER_SORT_DESCEND);
 $findCommand = $fm->newFindCommand($layouts[2]);
-$identificationID = $_GET["name"];
+if (isset($_GET['Genus'])) {
+    $identificationID = $_GET['Genus'];
+}
 $findCommand->addFindCriterion('Accession No.', $identificationID);
-$result = $findCommand->execute(); 
+$result = $findCommand->execute();
+
+// $result = $compoundFind->execute(); 
 $findAllRec = $result->getRecords();
 ?>
--- testThings/index.php
+++ testThings/index.php
@@ -6,11 +6,13 @@ require_once ('partials/header.php');
   <form action="render.php" method="get">
     <div class="form-group">
       <label>Accession No.</label>
-      <input type="text" name="name"><br>
+      <input type="text" name="Accession No."><br>
       <label>Genus</label>
       <input type="text" name="Genus"><br>
       <label>Species</label>
       <input type="text" name="Species"><br>
+      <label>Location</label>
+      <input type="text" name="Location"><br>
       <input class="btn btn-primary" type="submit">
     </div>
   </form>

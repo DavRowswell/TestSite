0c674300d0364e369e7896a5726a39748b71beba
qstevens <32005151+qstevens@users.noreply.github.com>
2018-12-12 14:21:11 -0800

    allow searching on input layout

--- testThings/functions.php
+++ testThings/functions.php
@@ -4,12 +4,16 @@ require_once ('databases/db.php');
 
 $fm = new FileMaker($FM_FILE, $FM_HOST, $FM_USER, $FM_PASS);
 
-echo $FM_HOST . "<br>";
-echo $FM_FILE . "<br>";
-echo $FM_USER . "<br>";
-echo $FM_PASS . "<br>";
+// echo $FM_HOST . "<br>";
+// echo $FM_FILE . "<br>";
+// echo $FM_USER . "<br>";
+// echo $FM_PASS . "<br>";
 
 $layouts = $fm->listLayouts();
+$findCommand = $fm->newFindCommand($layouts[2]);
+$result = $findCommand->execute();
+$findAllRec = $result->getRecords();
+$recFields = $result->getFields();
 
 if (FileMaker::isError($layouts)) {
     echo $layouts;
@@ -18,22 +22,27 @@ if (FileMaker::isError($layouts)) {
 // CompoundFind on all inputs with values
 $findCommand = $fm->newFindCommand($layouts[2]);
 
-if (isset($_GET['AccessionNo']) && $_GET['AccessionNo'] !== '') {
+foreach ($recFields as $rf) {
+    $field = explode(' ',trim($rf))[0];
+    if (isset($_GET[$field]) && $_GET[$field] !== '') {
     // echo "accession";
-    $findCommand->addFindCriterion('Accession No.', $_GET['AccessionNo']);
+    $findCommand->addFindCriterion($rf, $_GET[$field]);
 }
-if (isset($_GET['Genus']) && $_GET['Genus'] !== '') {
-    // echo "genus";
-    $findCommand->addFindCriterion('Genus', $_GET['Genus']);
 }
-if (isset($_GET['Species']) && $_GET['Species'] !== '') {
-    // echo "species";
-    $findCommand->addFindCriterion('Species', $_GET['Species']);
-}
-if (isset($_GET['Location']) && $_GET['Location'] !== '') {
-    // echo "location";
-    $findCommand->addFindCriterion('Location', $_GET['Location']);
-}
+
+
+// if (isset($_GET['Genus']) && $_GET['Genus'] !== '') {
+//     // echo "genus";
+//     $findCommand->addFindCriterion('Genus', $_GET['Genus']);
+// }
+// if (isset($_GET['Species']) && $_GET['Species'] !== '') {
+//     // echo "species";
+//     $findCommand->addFindCriterion('Species', $_GET['Species']);
+// }
+// if (isset($_GET['Location']) && $_GET['Location'] !== '') {
+//     // echo "location";
+//     $findCommand->addFindCriterion('Location', $_GET['Location']);
+// }
 
 $result = $findCommand->execute();
 
--- testThings/index.php
+++ testThings/index.php
@@ -1,22 +1,38 @@
 <?php 
+require_once ('FileMaker.php');
 require_once ('partials/header.php');
+require_once ('databases/db.php');
+
+$fm = new FileMaker($FM_FILE, $FM_HOST, $FM_USER, $FM_PASS);
+
+$layouts = $fm->listLayouts();
+$findCommand = $fm->newFindCommand($layouts[2]);
+$result = $findCommand->execute();
+$findAllRec = $result->getRecords();
+$recFields = $result->getFields();
+
+// foreach ($recFields as $rf) {
+//   echo $rf . " ";
+// }
 ?>
 
 <body class="container">
   <form action="render.php" method="get">
     <div class="form-group">
       <input type="text" name="Database" style="display:none;" 
-      value=<?php if (isset($_GET['db'])) echo $_GET['db'] ?>>
+      value=<?php if (isset($_GET['db'])) echo $_GET['db']; ?>>
     </div>
+    <?php foreach ($recFields as $rf) { ?>
     <div class="row">
       <div class="col-sm-2">
-      <label>Accession No.</label>
+      <label><?php echo $rf ?></label>
       </div>
       <div class="col-sm-2">
-      <input type="text" name="AccessionNo">
+      <input type="text" name=<?php echo $rf ?>>
       </div>
-    </div>
-    <div class="row">
+    </div> 
+    <?php } ?>
+    <!-- <div class="row">
       <div class="col-sm-2">
       <label>Genus</label>
       </div>
@@ -39,7 +55,7 @@ require_once ('partials/header.php');
       <div class="col-sm-2">
       <input type="text" name="Location">
       </div>
-    </div>
+    </div> -->
       <input class="btn btn-primary" type="submit">
     </div>
   </form>

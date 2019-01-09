c97be14205a5052534475bcbc756a28e86982011
h8p1b <stevensqiu@gmail.com>
2019-01-09 13:14:10 -0800

    edit comments

--- testThings/functions.php
+++ testThings/functions.php
@@ -9,28 +9,22 @@ $layout = "";
 foreach ($layouts as $l) {
     if (strpos($l, 'search') !== false) {
         $layout = $l;
-      }
+    }
 }
 
 $fmLayout = $fm->getLayout($layout);
 $layoutFields = $fmLayout->listFields();
 
-// $findCommand = $fm->newFindCommand($layout);
-// $result = $findCommand->execute();
-// $findAllRec = $result->getRecords();
-// $recFields = $result->getFields();
-
 if (FileMaker::isError($layouts)) {
     echo $layouts;
 }
 
-// CompoundFind on all inputs with values
+// Find on all inputs with values
 $findCommand = $fm->newFindCommand($layout);
 
 foreach ($layoutFields as $rf) {
     $field = explode(' ',trim($rf))[0];
     if (isset($_GET[$field]) && $_GET[$field] !== '') {
-        // echo "accession";
         $findCommand->addFindCriterion($rf, $_GET[$field]);
     }
 }

3d8afaa5514588adf11d0c55f037a1fd985cf133
crystal-ck7 <crystal.ck7@gmail.com>
2019-01-23 11:28:11 -0800

    added dropdown menus

--- testThings/partials/header.php
+++ testThings/partials/header.php
@@ -25,16 +25,32 @@
       <a class="navbar-brand" href="#">HerbariumDB</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="<?php echo(active("index.php?Database=miw"))?>"><a href="index.php?Database=miw">Marine Invertebrates</a></li>
-      <li class="<?php echo(active("index.php?Database=bryophytes"))?>"><a href="index.php?Database=bryophytes">Bryophytes</a></li>
-      <li class="<?php echo(active("index.php?Database=fungi"))?>"><a href="index.php?Database=fungi">Fungi</a></li>
-      <li class="<?php echo(active("index.php?Database=lichen"))?>"><a href="index.php?Database=lichen">Lichen</a></li>
-      <li class="<?php echo(active("index.php?Database=algae"))?>"><a href="index.php?Database=algae">Algae</a></li>
-      <li class="<?php echo(active("index.php?Database=vwsp"))?>"><a href="index.php?Database=vwsp">Vascular Plants</a></li>
-      <li class="<?php echo(active("index.php?Database=avian"))?>"><a href="index.php?Database=avian">Avian</a></li>
-      <li class="<?php echo(active("index.php?Database=fish"))?>"><a href="index.php?Database=fish">Fish</a></li>
-      <li class="<?php echo(active("index.php?Database=herpetology"))?>"><a href="index.php?Database=herpetology">Herpetology</a></li>
-      <li class="<?php echo(active("index.php?Database=mammal"))?>"><a href="index.php?Database=mammal">Mammal</a></li>
+      <li class="nav-item dropdown">
+         <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Herbarium <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li class="<?php echo(active("index.php?Database=algae"))?>"><a href="index.php?Database=algae">Algae</a></li>
+              <li class="<?php echo(active("index.php?Database=bryophytes"))?>"><a href="index.php?Database=bryophytes">Bryophytes</a></li>
+              <li class="<?php echo(active("index.php?Database=fungi"))?>"><a href="index.php?Database=fungi">Fungi</a></li>
+              <li class="<?php echo(active("index.php?Database=lichen"))?>"><a href="index.php?Database=lichen">Lichen</a></li>
+              <li class="<?php echo(active("index.php?Database=vwsp"))?>"><a href="index.php?Database=vwsp">Vascular Plants</a></li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+         <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Vertebrates <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li class="<?php echo(active("index.php?Database=avian"))?>"><a href="index.php?Database=avian">Avian</a></li>
+              <li class="<?php echo(active("index.php?Database=fish"))?>"><a href="index.php?Database=fish">Fish</a></li>
+              <li class="<?php echo(active("index.php?Database=herpetology"))?>"><a href="index.php?Database=herpetology">Herpetology</a></li>
+              <li class="<?php echo(active("index.php?Database=mammal"))?>"><a href="index.php?Database=mammal">Mammal</a></li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+         <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Invertebrates <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+                   <li class="<?php echo(active("index.php?Database=miw"))?>"><a href="index.php?Database=miw">Marine Invertebrates</a></li>
+            </ul>
+        </li>
+      </li>
     </ul>
   </div>
 </nav>

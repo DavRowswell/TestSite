9873836087f64ce68fcd517eb998d214f31560e4
David Rowswell <davi144@mail.ubc.ca>
2019-02-20 15:52:33 -0800

    Added left passing to main page and cleaned some footer up

--- testThings/partials/footer.php
+++ testThings/partials/footer.php
@@ -3,7 +3,7 @@
     <br>
     <hr align = "center" style = "width: 100%; border: 1px solid #969B9B;">
     <div style = "font-style: italic; text-align: center;"> 
-        If there are bugs, send email to davi144@mail.ubc.ca 
+        If there are bugs, send an email to davi144@mail.ubc.ca 
         <br> 
         <span style = "font-style: normal; font-size: 0.8em;"> 
             *This site is still in alpha and can have some critical bugs.

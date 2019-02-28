13017e9a661ff02d82dcb65227db68239bd8dba3
David Rowswell <davi144@mail.ubc.ca>
2019-02-28 12:50:04 -0800

    Changed title to better suit the page reason

--- testSite/partials/header.php
+++ testSite/partials/header.php
@@ -1,5 +1,5 @@
 <!-- Bootstrap CSS -->
-<title>Bootstrap Example</title>
+<title>Collection Search</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="css/css/all.css">

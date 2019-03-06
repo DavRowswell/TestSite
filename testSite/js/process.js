268dfd6ed4be4a2e910c29d3eb2c1282e9fae9d4
David Rowswell <davi144@mail.ubc.ca>
2019-03-05 17:52:15 -0800

    removed let to var so that older browsers do not ahve an issue

--- testSite/js/process.js
+++ testSite/js/process.js
@@ -1,13 +1,13 @@
 
 function Process() {
-    let vals = document.getElementById("submit-form").children;
+    var vals = document.getElementById("submit-form").children;
     console.log(vals[3]);
     document.getElementById("submit-form").submit();
 }
 
 function clearURL() {
-    let vals = document.getElementById("submit-form");
-    let inputs = vals.querySelectorAll("input[type=text]");
+    var vals = document.getElementById("submit-form");
+    var inputs = vals.querySelectorAll("input[type=text]");
     for(var i=0;i< inputs.length;i++){
         if(inputs[i].value.length == 0){
             inputs[i].parentNode.removeChild(inputs[i]);
@@ -25,8 +25,11 @@ function keyPress(e){
   }
 }
 
+window.onload = pageReload;
+
 function pageReload(){
-    location.reload(true);
+    
+    //location.reload(true);
 }
 
 

1cae5e76d3f708a4b1b7aade69c956c67d335334
crystal-ck7 <crystal.ck7@gmail.com>
2019-03-08 19:12:18 -0800

    added in map stuff to show a point, it's a oval!

--- testSite/js/map.js
+++ testSite/js/map.js
@@ -1,12 +1,66 @@
-    require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
+    require(["esri/Map", "esri/views/MapView", "esri/layers/GraphicsLayer", "esri/Graphic",
+     "esri/geometry/Point",  "esri/geometry/Circle", "esri/geometry/SpatialReference"], 
+    function(Map, MapView, GraphicsLayer, Graphic, Point, Circle, SpatialReference) {
     var map = new Map({
-        basemap: "streets",  //For full list of pre-defined basemaps, navigate to http://arcg.is/1JVo6Wd
+        basemap: "topo",  //For full list of pre-defined basemaps, navigate to http://arcg.is/1JVo6Wd
     });
 
     var view = new MapView({
         container: "map",
         map: map,
-        center: [15, 65], // longitude, latitude
-        zoom: 4
+        center: [document.getElementById("Longitude").innerHTML, document.getElementById("Latitude").innerHTML], // longitude, latitude
+        zoom: 12,
+        spatialReference: {wkid: 4326}
     });
+
+      // create a point
+      var point = {
+        type: "point",  // autocasts as new point()
+        latitude: document.getElementById("Latitude").innerHTML,
+        longitude: document.getElementById("Longitude").innerHTML
+      };
+
+      // create a circle with point and radius
+      var circle = new Circle({
+        center: point,
+        radius: 8000,
+        geodesic: true
+      });
+      
+       // Create a symbol for drawing the point
+      var markerSymbol = {
+        type: "simple-marker",  // autocasts as new SimpleMarkerSymbol()
+        color: [255, 51, 51]
+        };
+  
+        // Create a graphic and add the geometry and symbol to it
+        var graphicA = new Graphic({
+            geometry: point,
+            symbol: markerSymbol
+        });
+
+         // Create a symbol for drawing the point
+      var fillMarkerSymbol = {
+        type: "simple-fill",  // autocasts as new SimpleMarkerSymbol()
+        color: [255, 51, 51, 0.5],
+        style: "solid",
+        outline: {
+            color: "black",
+            width: 1
+        }
+        };
+
+        var graphicB = new Graphic({
+            geometry: circle,
+            symbol: fillMarkerSymbol
+        });
+
+      // Add graphic when GraphicsLayer is constructed
+      var layer = new GraphicsLayer({
+        graphics: [graphicA, graphicB]
+      });
+    
+      // Add GraphicsLayer to map
+      map.add(layer);
+
     });

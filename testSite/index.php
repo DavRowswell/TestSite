1e32c7f00575664789e9dd76356f40ebb1c447b9
David Rowswell <davi144@mail.ubc.ca>
2019-03-27 19:47:18 -0700

    Updating the intro page

--- testSite/index.php
+++ testSite/index.php
@@ -30,7 +30,7 @@
         }
 
         #main {
-            margin-left: 2%;
+           /* margin-left: 2%;*/
         }
 
         .btn-beaty {
@@ -39,50 +39,88 @@
             border-color: #CC232A;
         }
         #someid {
-            height: 20%;
+           /* height: 20%;*/
         }
     </style>
 </head>
 <body class="container-fluid">
     <?php require_once ('partials/navbar.php'); ?>
     <div id="main">
-        <h1 style="position:relative; top:16px"> <b>Home Page </b> </h1>
-        <div id="herbarium-databases">
-        <h2 id = "herbarium"> <b>Herbarium Databases</b> </h2>
-        <hr align = "left">
-        <div id="herbarium-links">
-        <a href="https://herbweb.botany.ubc.ca/testSite/search.php?Database=algae"><figure><img class="img-fluid" id= "someid" src ="images/herbarium.jpg"><figcaption>Algae</figcaption></figure></a>
-        <a role="button" class="btn btn-danger" href="https://herbweb.botany.ubc.ca/testSite/search.php?Database=bryophytes">Bryophytes</a>
-        <a role="button" class="btn btn-danger" href="https://herbweb.botany.ubc.ca/testSite/search.php?Database=fungi">Fungi</a>
-        <a role="button" class="btn btn-danger" href="https://herbweb.botany.ubc.ca/testSite/search.php?Database=lichen">Lichen</a>
-        <a role="button" class="btn btn-danger" href="https://herbweb.botany.ubc.ca/testSite/search.php?Database=vwsp">Vascular</a>
+        <div id="main-title" class="row">
+            <div class="col">
+                <h1><b>Database List</b></h1>
+            </div>
         </div>
+        <div id="herbarium-title" class="row">
+            <div class="col">
+                <h2 id = "herbarium"><b>Herbarium Databases</b></h2>
+                <hr align = "left">
+            </div>
         </div>
-        <div id="vertebrate-databases">
-        <h2 id= "vertebrate"> <b>Vertebrate Databases</b> </h2>
-        <hr align = "left">
-        <div id="vertebrate-links">
-        <a role="button" class="btn btn-danger" href="search.php?Database=avian">Avian</a>
-        <a role="button" class="btn btn-danger" href="search.php?Database=herpetology">Herpetology</a>
-        <a role="button" class="btn btn-danger" href="search.php?Database=mammal">Mammals</a>
-        <a role="button" class="btn btn-danger" href="search.php?Database=fish">Fish</a>
+        <div id="herbarium-databases" class="row">
+            <div class="col-sm-1">
+                <a href="https://herbweb.botany.ubc.ca/testSite/search.php?Database=algae"><figure><img class="img-fluid" id= "someid" src ="images/herbarium.png"><figcaption style="text-align:center;">Algae</figcaption></figure></a>
+            </div>
+            <div class="col-sm-1">
+                <a href="https://herbweb.botany.ubc.ca/testSite/search.php?Database=bryophytes"><figure><img class="img-fluid" id= "someid" src ="images/herbarium.png"><figcaption style="text-align:center;">Bryophytes</figcaption></figure></a>
+            </div>
+            <div class="col-sm-1">
+                <a href="https://herbweb.botany.ubc.ca/testSite/search.php?Database=fungi"><figure><img class="img-fluid" id= "someid" src ="images/herbarium.png"><figcaption style="text-align:center;">Fungi</figcaption></figure></a>
+            </div>
+            <div class="col-sm-1">
+                <a href="https://herbweb.botany.ubc.ca/testSite/search.php?Database=lichen"><figure><img class="img-fluid" id= "someid" src ="images/herbarium.png"><figcaption style="text-align:center;">Lichen</figcaption></figure></a>
+            </div>
+            <div class="col-sm-1">
+                <a href="https://herbweb.botany.ubc.ca/testSite/search.php?Database=vwsp"><figure><img class="img-fluid" id= "someid" src ="images/herbarium.png"><figcaption style="text-align:center;">Vascular</figcaption></figure></a>
+            </div>
         </div>
+        <div id="vertabrae-title" class="row">
+            <div class="col">
+                <h2 id= "vertebrate"> <b>Vertebrate Databases</b> </h2>
+                <hr align = "left">
+            </div>
         </div>
-        <div id="invertebrate-databases">
-        <h2 id = "invertebrate"> <b>Invertebrate Databases</b> </h2>
-        <hr align = "left">
-        <div id="invertebrate-links">
-        <a role="button" class="btn btn-danger" href="search.php?Database=entomology">Entomology</a>
-        <a role="button" class="btn btn-danger" href="search.php?Database=miw">Wet Marine Invertebrates</a>
-        <a role="button" class="btn btn-danger" href="search.php?Database=mi">Dry Marine Invertebrates</a>
+        <div id="vertebrate-databases" class="row">
+            <div class="col-sm-1">
+                <a href="search.php?Database=avian"><figure><img class="img-fluid" id= "someid" src ="images/tetrapods.png"><figcaption style="text-align:center;">Avian</figcaption></figure></a>
+            </div>
+            <div class="col-sm-1">
+                <a href="search.php?Database=herpetology"><figure><img class="img-fluid" id= "someid" src ="images/tetrapods.png"><figcaption style="text-align:center;">Herpetology</figcaption></figure></a>
+            </div>
+            <div class="col-sm-1">
+                <a href="search.php?Database=mammal"><figure><img class="img-fluid" id= "someid" src ="images/tetrapods.png"><figcaption style="text-align:center;">Mammals</figcaption></figure></a>
+            </div>
+            <div class="col-sm-1">
+                <a href="search.php?Database=fish"><figure><img class="img-fluid" id= "someid" src ="images/fish.png"><figcaption style="text-align:center;">Fish</figcaption></figure></a>
+            </div>
         </div>
+        <div id="invertebrate-title" class="row">
+            <div class="col">
+                <h2 id = "invertebrate"> <b>Invertebrate Databases</b> </h2>
+                <hr align = "left">
+            </div>
+        </div>
+        <div id="invertebrate-databases" class="row">
+            <div class="col-sm-1">
+                <a href="search.php?Database=entomology"><figure><img class="img-fluid" id= "someid" src ="images/entomology.png"><figcaption style="text-align:center;">Entomology</figcaption></figure></a>
+            </div>
+            <div class="col-sm-1">
+                <a href="search.php?Database=miw"><figure><img class="img-fluid" id= "someid" src ="images/marine-invertebrates.png"><figcaption style="text-align:center;">Wet Marine Invertebrates</figcaption></figure></a>
+            </div>
+            <div class="col-sm-1">
+                <a href="search.php?Database=mi"><figure><img class="img-fluid" id= "someid" src ="images/marine-invertebrates.png"><figcaption style="text-align:center;">Dry Marine Invertebrates</figcaption></figure></a>
+            </div>
+        </div>
+        <div id="bone-title" class="row">
+            <div class="col">
+                <h2 id = "bone"> <b>Fossil Database</b> </h2>
+                <hr align = "left">
+            </div>
         </div>
         <div id="bone-databases">
-        <h2 id = "bone"> <b>Fossil Database</b> </h2>
-        <hr align = "left">
-        <div id="bone-links">
-        <a role="button" class="btn btn-danger" href="search.php?Database=fossil">Fossils</a>
-        </div>
+            <div class="col-sm-1">
+                <a href="search.php?Database=fossil"><figure><img class="img-fluid" id= "someid" src ="images/fossils.png"><figcaption style="text-align:center;">Fossils</figcaption></figure></a>
+            </div>
         </div>
     </div>
     <?php require_once("partials/footer.php");?>

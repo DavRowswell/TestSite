12a56353f6bf72b1b30661ed0f3648f06014b834
David Rowswell <davi144@mail.ubc.ca>
2019-04-02 13:02:35 -0700

    Fixed some alyout for the search page

--- testSite/search.php
+++ testSite/search.php
@@ -53,7 +53,7 @@
     </div>
   </div>
   <div class ="row">
-    <div id="form" class = "col-sm-4">
+    <div id="form" class = "col-sm-5">
       <form action="render.php" method="get" id = "submit-form">
         <div class="form-group">
           <input type="text" name="Database" style="display:none;" 
@@ -83,12 +83,12 @@
             $ignoreValues = ['SortNum', 'AccessionNumerical', 'Imaged', 'IIFRNo', 'Photographs::photoFileName'];
             if (in_array($rf, $ignoreValues)) continue; ?>
           <div class="row">
-            <div class="col">
+            <div class="col-sm-6">
               <label for="field-<?php echo $rf?>">
                 <?php echo htmlspecialchars(formatField($rf)) ?>
               </label>
             </div>
-            <div class="col">   
+            <div class="col-sm-6">   
               <input type="text" id="field-<?php echo $rf?>" 
               <?php
                 if (isset($_POST[str_replace(' ', '_', $rf)]))
@@ -99,26 +99,9 @@
             </div>
           </div> 
           <?php } ?>
-        <div class = "row" style="padding-top:15px">
-          <div class = "col" style ="font-size:13px"> Search By
-            <div class = "btn-group btn-group-toggle" data-toggle="buttons" >
-              <label class = "btn btn-primary active" style="font-size:12px;">
-                <input type="radio"  id = "and" autocomplete="off"  checked> AND 
-              </label>
-              <label class = "btn btn-primary" style="font-size:12px;">
-                <input type="radio" id = "or" autocomplete="off" > <span style="visibility: hidden">&nbsp;</span>OR<span style="visibility: hidden">&nbsp;</span>
-              </label> 
-            </div>
-          </div>
-          <div class="col">
-            <a href="render.php?Database=<?php echo htmlspecialchars($_GET['Database'])?>" 
-                role="button" class="btn btn-primary" 
-                style="font-size:12px; text-align:left; padding-left:2px; padding-right:2px;">Show All Records</a>   
-          </div>
-        </div>
       </form>
     </div>
-    <div class="col-sm-2"></div>
+    <div class="col-sm-1"></div>
     <div id="legend" class="border col-sm-5"> 
       <?php
       if($_GET['Database'] === 'entomology'){
@@ -129,18 +112,19 @@
         echo '</div>';
       }
       ?>
-      <header style="padding-bottom:12px"> Search Operators </header>
       <div class="row">
-        <div class="col-sm-1"> = </div>
-        <div class="col-sm-11"> match a whole word (or match empty) </div>
+        <div class="col" style="text-align:center;">
+          <h2 style="padding-bottom:12px"> Search Options </h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <h3 style="padding-bottom:12px"> Search Operators </h3>
+        </div>
       </div>
       <div class="row">
         <div class="col-sm-1"> == </div>
         <div class="col-sm-11"> match entire field exactly </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-1"> ! </div>
-        <div class="col-sm-11"> find duplicate values </div>
       </div>
       <div class="row">
         <div class="col-sm-1"> &lt </div>
@@ -163,22 +147,6 @@
         <div class="col-sm-11">  find records with values in a range (Ex. 10...20) </div>
       </div>
       <div class="row">
-        <div class="col-sm-1"> &frasl;&frasl; </div>
-        <div class="col-sm-11">  find records with today's date </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-1"> ? </div>
-        <div class="col-sm-11">  find records invalid date and time </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-1"> @ </div>
-        <div class="col-sm-11">  match any one character </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-1"> # </div>
-        <div class="col-sm-11">  match any digit </div>
-      </div>
-      <div class="row">
         <div class="col-sm-1"> * </div>
         <div class="col-sm-11">  match zero or more characters </div>
       </div>
@@ -186,13 +154,29 @@
         <div class="col-sm-1"> \ </div>
         <div class="col-sm-11">  escape any character </div>
       </div>
-      <div class="row">
-        <div class="col-sm-1"> &#34&#34 </div>
-        <div class="col-sm-11">  match phrase from word start </div>
+      <div class = "row">
+        <div class = "col"> 
+          <h4 style=padding-top:12px;>Search By</h4>
+        </div>
       </div>
-      <div class="row">
-        <div class="col-sm-1"> *&#34&#34 </div>
-        <div class="col-sm-11">  match phrase from anywhere </div>
+      <div class = "row">
+        <div class="col">
+          <div class = "btn-group btn-group-toggle" data-toggle="buttons" >
+            <label class = "btn btn-primary active" style="font-size:12px;">
+              <input type="radio"  id = "and" autocomplete="off"  checked> AND 
+            </label>
+            <label class = "btn btn-primary" style="font-size:12px;">
+              <input type="radio" id = "or" autocomplete="off" > <span style="visibility: hidden">&nbsp;</span>OR<span style="visibility: hidden">&nbsp;</span>
+            </label> 
+          </div>
+        </div>
+      </div>
+      <div class="row" style="padding-top:12px;">
+        <div class="col">
+          <a href="render.php?Database=<?php echo htmlspecialchars($_GET['Database'])?>" 
+              role="button" class="btn btn-primary" 
+              style="font-size:12px; text-align:left; padding-left:2px; padding-right:2px;">Show All Records</a>   
+        </div>
       </div>
     </div>
   </div>

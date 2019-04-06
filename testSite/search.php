cb844b6109823893b3c3516cdc46e29bdf4061c7
David Rowswell <davi144@mail.ubc.ca>
2019-04-05 19:14:27 -0700

    Fixed formatting of the search page

--- testSite/search.php
+++ testSite/search.php
@@ -52,9 +52,9 @@
         <h1><b><?php echo ucfirst($_GET['Database']); ?> Search</b></h1>
     </div>
   </div>
-  <div class ="row">
-    <div id="form" class = "col-sm-5">
-      <form action="render.php" method="get" id = "submit-form">
+  <form action="render.php" method="get" id = "submit-form">
+    <div class ="row">
+      <div id="form" class = "col-sm-6">
         <div class="form-group">
           <input type="text" name="Database" style="display:none;" 
           value=<?php if (isset($_GET['Database'])) echo htmlspecialchars($_GET['Database']); ?>>
@@ -64,31 +64,23 @@
             <input id="form" class="btn btn-primary" type="button" value="Submit"  style = "font-size:12px;" onclick="Process(clearURL())">    
           </div>     
         </div>
-        <?php if ($_GET['Database'] == 'fish' || $_GET['Database'] == 'avian' ||$_GET['Database'] == 'herpetology' || $_GET['Database'] == 'mammal'
-        || $_GET['Database'] == 'vwsp' || $_GET['Database'] == 'bryophytes' || $_GET['Database'] == 'entomology' ||
-        $_GET['Database'] == 'fungi' || $_GET['Database'] == 'lichen' || $_GET['Database'] == 'algae') { ?>
-          <div class="col-sm-12 form-check">
-                <input class="form-check-input" type="checkbox" value="" id="imageCheck">
-                <label class="form-check-label" for="imageCheck">
-                    Only show records that contain an image
-                </label>
-          </div>
-          <input type="hidden" name = "hasImage" id = "hasImage">
-        <?php } ?>
-        <input type="hidden" name = "type" id = "type">
         <br>   
         <?php 
+          $count = 0;
           foreach ($layoutFields as $rf) {
             //echo $rf;
             $ignoreValues = ['SortNum', 'AccessionNumerical', 'Imaged', 'IIFRNo', 'Photographs::photoFileName', 'Event::eventDate'];
-            if (in_array($rf, $ignoreValues)) continue; ?>
+            if (in_array($rf, $ignoreValues)) continue; 
+            if($count%2==0) {?>
           <div class="row">
-            <div class="col-sm-6">
+            <?php }?>
+            <!--- Section that is one label and one search box --->
+            <div class="col-sm-3">
               <label for="field-<?php echo $rf?>">
                 <?php echo htmlspecialchars(formatField($rf)) ?>
               </label>
             </div>
-            <div class="col-sm-6">   
+            <div class="col-sm-3">   
               <input type="text" id="field-<?php echo $rf?>" 
               <?php
                 if (isset($_POST[str_replace(' ', '_', $rf)]))
@@ -97,89 +89,109 @@
               name="<?php echo htmlspecialchars($rf) ?>"
               class="form-control">
             </div>
-          </div> 
-          <?php } ?>
-      </form>
-    </div>
-    <div class="col-sm-1"></div>
-    <div id="legend" class="border col-sm-5"> 
-      <?php
-      if($_GET['Database'] === 'entomology'){
-        echo '<div class="row">';
-          echo '<div class="col-sm-12">';
-            echo '<a href="https://www.zoology.ubc.ca/entomology/"><img width="100%" src="images/entomology-link-image.jpg"></a>';
+            <!--- End of a single label, input instance --->
+            <?php if($count%2==1) {?>
+          </div>
+            <?php }?>
+        <?php $count++; } 
+          if($count%2==1) {
+            echo '</div>';
+          }
+        ?>
+      </div>
+      <div id="legend" class="border col-sm-5"> 
+        <?php
+        if($_GET['Database'] === 'entomology'){
+          echo '<div class="row">';
+            echo '<div class="col-sm-12">';
+              echo '<a href="https://www.zoology.ubc.ca/entomology/"><img width="100%" src="images/entomology-link-image.jpg"></a>';
+            echo '</div>';
           echo '</div>';
-        echo '</div>';
-      }
-      ?>
-      <div class="row">
-        <div class="col" style="text-align:center;">
-          <h2 style="padding-bottom:12px"> Search Options </h2>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col">
-          <h3 style="padding-bottom:12px"> Search Operators </h3>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-1"> == </div>
-        <div class="col-sm-11"> match entire field exactly </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-1"> &lt </div>
-        <div class="col-sm-11"> find records with values less than to the one specified </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-1"> &lt= </div>
-        <div class="col-sm-11">  find records with values less than or equal to the one specified </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-1"> &gt </div>
-        <div class="col-sm-11">  find records with values greater than to the one specified </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-1"> &gt= </div>
-        <div class="col-sm-11">  find records with values greater than or equal to the one specified </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-1"> ... </div>
-        <div class="col-sm-11">  find records with values in a range (Ex. 10...20) </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-1"> * </div>
-        <div class="col-sm-11">  match zero or more characters </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-1"> \ </div>
-        <div class="col-sm-11">  escape any character </div>
-      </div>
-      <div class = "row">
-        <div class = "col"> 
-          <h4 style=padding-top:12px;>Search By</h4>
-        </div>
-      </div>
-      <div class = "row">
-        <div class="col">
-          <div class = "btn-group btn-group-toggle" data-toggle="buttons" >
-            <label class = "btn btn-primary active" style="font-size:12px;">
-              <input type="radio"  id = "and" autocomplete="off"  checked> AND 
-            </label>
-            <label class = "btn btn-primary" style="font-size:12px;">
-              <input type="radio" id = "or" autocomplete="off" > <span style="visibility: hidden">&nbsp;</span>OR<span style="visibility: hidden">&nbsp;</span>
-            </label> 
+        }
+        ?>
+        <div class="row">
+          <div class="col" style="text-align:center;">
+            <h2 style="padding-bottom:12px"> Search Options </h2>
           </div>
         </div>
-      </div>
-      <div class="row" style="padding-top:12px;">
-        <div class="col">
-          <a href="render.php?Database=<?php echo htmlspecialchars($_GET['Database'])?>" 
-              role="button" class="btn btn-primary" 
-              style="font-size:12px; text-align:left; padding-left:2px; padding-right:2px;">Show All Records</a>   
+        <div class="row">
+          <div class="col">
+            <h3 style="padding-bottom:12px"> Search Operators </h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-1"> == </div>
+          <div class="col-sm-11"> match entire field exactly </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-1"> &lt </div>
+          <div class="col-sm-11"> find records with values less than to the one specified </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-1"> &lt= </div>
+          <div class="col-sm-11">  find records with values less than or equal to the one specified </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-1"> &gt </div>
+          <div class="col-sm-11">  find records with values greater than to the one specified </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-1"> &gt= </div>
+          <div class="col-sm-11">  find records with values greater than or equal to the one specified </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-1"> ... </div>
+          <div class="col-sm-11">  find records with values in a range (Ex. 10...20) </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-1"> * </div>
+          <div class="col-sm-11">  match zero or more characters </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-1"> \ </div>
+          <div class="col-sm-11">  escape any character </div>
+        </div>
+        <div class = "row">
+          <div class = "col"> 
+            <h4 style=padding-top:12px;>Search By</h4>
+          </div>
+        </div>
+        <div class = "row">
+          <div class="col">
+            <div class = "btn-group btn-group-toggle" data-toggle="buttons" >
+              <label class = "btn btn-primary active" style="font-size:12px;">
+                <input type="radio"  id = "and" autocomplete="off"  checked> AND 
+              </label>
+              <label class = "btn btn-primary" style="font-size:12px;">
+                <input type="radio" id = "or" autocomplete="off" > <span style="visibility: hidden">&nbsp;</span>OR<span style="visibility: hidden">&nbsp;</span>
+              </label> 
+            </div>
+          </div>
+        </div>
+        <div class="row" style="padding-top:12px;">
+          <div class="col">
+            <a href="render.php?Database=<?php echo htmlspecialchars($_GET['Database'])?>" 
+                role="button" class="btn btn-primary" 
+                style="font-size:12px; text-align:left; padding-left:2px; padding-right:2px;">Show All Records</a>   
+          </div>
+        </div>
+        <div class="row" style="padding-top:12px;">
+          <?php if ($_GET['Database'] == 'fish' || $_GET['Database'] == 'avian' ||$_GET['Database'] == 'herpetology' || $_GET['Database'] == 'mammal'
+          || $_GET['Database'] == 'vwsp' || $_GET['Database'] == 'bryophytes' || $_GET['Database'] == 'entomology' ||
+          $_GET['Database'] == 'fungi' || $_GET['Database'] == 'lichen' || $_GET['Database'] == 'algae') { ?>
+            <div class="col">
+              <input type="checkbox" value="" id="imageCheck">
+              <label for="imageCheck">
+                Only show records that contain an image
+              </label>
+            </div>
+            <input type="hidden" name = "hasImage" id = "hasImage">
+          <?php } ?>
+          <input type="hidden" name = "type" id = "type">
         </div>
       </div>
     </div>
-  </div>
+  </form>
   <?php require_once("partials/footer.php");?>
   <script src="js/process.js"> </script>
 </body>

f8768ef95d03ef402d8f3d5b2622f4b3d7dce15c
David Rowswell <davi144@mail.ubc.ca>
2019-04-16 13:57:01 -0700

    removing Herbarium Site from the repo since I think we are past that stage

--- Herbarium_Site/index.php
+++
@@ -1,8 +0,0 @@
-<?php include 'includes/header.php'; ?>
-<img src="http://www.botany.ubc.ca/herbarium/straley/images/mainban01.jpg">
-        <font class="h1">
-        <br><b>What is a Herbarium?</b>
-<p>A herbarium is a museum of dried plant specimens. Herbaria provide a permanent record of our changing flora over time, documenting all plant species discovered so far, their variation, and their past and present distributions. These specimens also hold a treasure trove of anatomical, chemical, ethnobotanical, and molecular information, and document the history of plant exploration. 
-		<p>&nbsp;
-	    <p>&nbsp;
-<?php include 'includes/footer.php'; ?>

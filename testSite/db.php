77bbc1924e6f3ba8c8317eb4d55d447d726a4bc8
David Rowswell <davi144@mail.ubc.ca>
2019-04-23 13:40:46 -0700

    made it go to error page instead

--- testSite/db.php
+++ testSite/db.php
@@ -1,10 +1,17 @@
 <?php
 	if (isset($_GET['Database']) && $_GET['Database'] != "") {
-		if ($_GET['Database'] !== 'all' ||
+		if ($_GET['Database'] !== 'all' && (
 		$_GET['Database'] == 'avian' ||$_GET['Database'] == 'herpetology' || $_GET['Database'] == 'mammal' ||
 		$_GET['Database'] == 'vwsp' || $_GET['Database'] == 'bryophytes' || $_GET['Database'] == 'fungi' || $_GET['Database'] == 'lichen' || $_GET['Database'] == 'algae'||
-		$_GET['Database'] === 'entomology' || $_GET['Database'] === 'fish' || $_GET['Database'] === 'mi' || $_GET['Database'] === 'fossil')
-		require_once ('databases/'.$_GET['Database'].'db.php');
+		$_GET['Database'] === 'entomology' || $_GET['Database'] === 'fish' || $_GET['Database'] === 'mi' || $_GET['Database'] === 'fossil')) {
+			require_once ('databases/'.$_GET['Database'].'db.php');
+		}
+		else {
+			session_start();
+			$_SESSION['error'] = "Not a valid database given.";
+			header('Location: error.php');
+			exit;
+		}
 	} else {
 		require_once ('databases/miwdb.php');
 	}

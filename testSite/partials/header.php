0f3465cb76ee2ed01730bb898a7e92bb2148a31b
David Rowswell <davi144@mail.ubc.ca>
2019-05-10 13:21:52 -0700

    fixing firefox css

--- testSite/partials/header.php
+++ testSite/partials/header.php
@@ -2,10 +2,10 @@
 <title>Collection Search</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="css/open-iconic-master/font/css/open-iconic-bootstrap.css" rel="stylesheet">
-<link rel="stylesheet" href="css/reset.css">
-<link rel="stylesheet" href="css/bootstrap.css">
-<link rel="stylesheet" href="css/allPagesCustom.css">
+<link href="css/open-iconic-master/font/css/open-iconic-bootstrap.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/allPagesCustom.css">
 <script src="js/jquery.js"></script>
 <script src="js/popper.js"></script>
 <script src="js/bootstrap.js"></script>
